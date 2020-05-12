4a1091ac2a2b6e68c0f62352b3e39e140cd696ba
zzunny97 <zzunny97@gmail.com>
2020-05-12 23:57:02 +0900

    .

--- PA2/new/BTree.h
+++ PA2/new/BTree.h
@@ -40,6 +40,49 @@ class BTreeNode {
 		NodeType getNodeType() {
 			return type;
 		}
+
+		BTreeNode* sibling(bool is_left) {
+			BTreeNode* parent = this->parent;
+			if(parent == NULL) {
+				cout << "[ERROR] BTreeNode::sibling(), parent not defined" << endl;
+				exit(1);
+			}
+			int i;
+			for(i=0; i<parent->num_keys+1; i++) {
+				if(parent->child[i] == this) {
+					break;
+				}
+			}
+			if(i==parent->num_keys+1) {
+				cout << "[ERROR] BTreeNode::sibling(), child not found" << endl;
+				exit(1);
+			}
+			if(is_left) {
+				// find left sibling
+				if(i>0) return this->parent->child[i-1];
+			}
+			else {
+				// find right sibling
+				if(i<parent->num_keys) return this->parent->child[i+1];
+			}
+			return NULL;
+		}
+
+		bool too_few() {
+			if(type == LEAF) {
+				// leaf
+				int num_val = num_keys;
+				if(num_val < ceil((float)(NUM_KEYS-1)/2)) return true;
+			}
+			else {
+				// nonleaf
+				int num_ptr = num_keys+1;
+				if(num_ptr < ceil((float)(NUM_KEYS)/2))
+					return true;
+			}
+			return false;
+		}
+
 		void printLeafNode() {
 			/*
 			if(type != LEAF) {
@@ -132,12 +175,14 @@ class BTree {
 			BTreeNode* p = n1->parent;
 			if(p->num_keys+1 < NUM_KEYS) {
 				// insert value and pointer n2 into p right after n1
+				//cout << "room available in parent" << endl;
 				for(int i=0; i<p->num_keys+1; i++) {
 					if(p->child[i] == n1) {
 						for(int j=p->num_keys-1; j>=i; j--) p->keys[j+1] = p->keys[j]; // move keys backward
 						for(int j=p->num_keys; j>=i+1; j--) p->child[j+1] = p->child[j]; // move childs backward
 						p->keys[i] = value;
 						p->child[i+1] = n2;
+						p->num_keys++;
 						n2->parent = p;
 						break;
 					}
@@ -226,6 +271,7 @@ class BTree {
 					}
 				}
 				int smallest_key_of_newleaf = newleaf->keys[0];
+				//cout << "smallest_key_of_newleaf: " << smallest_key_of_newleaf << endl;
 				//cout << "function : insert_in_parent" << endl;
 				insert_in_parent(cur, smallest_key_of_newleaf, newleaf);
 			}
@@ -234,8 +280,59 @@ class BTree {
 			//cout << "insert end" << endl;
 		}
 
+		void delete_entry(BTreeNode* n, long long value) {
+			// delete key from node n
+			for(int i=0; i<n->num_keys; i++) {
+				if(n->keys[i] == value) {
+					for(int j=i; j<n->num_keys; j++) {
+						n->keys[j] = n->keys[j+1];	
+					}
+					n->num_keys--;
+				}
+			}
+			if(n->type == ROOT && n->num_keys==0) {
+				// make the child of n the new root
+				// and delete n
+				root = n->child[0];
+				delete n;
+			}
+			else if(n->too_few()) {
+				BTreeNode* left_sibling = n->sibling(true); // true means left sibling flag
+				BTreeNode* right_sibling = n->sibling(false); // false means right sibling flag
+				if(n->CanMerge(left_sibling) || n->CanMerge(right_sibling)) {
+					
+				}
+			}
+		}
+
 		void remove(long long value) {
 			cout << "[INST]: REMOVE: " << value << endl;
+			BTreeNode* cur = root;
+			while(cur->getNodeType() != LEAF) {
+				if(depth==1)  break;
+				int i = cur->find_idx(value);
+				if(i==cur->num_keys) {
+					cur = cur->find_last_non_null();
+					return;
+				}
+				else if(value == cur->keys[i]) {
+					cur = cur->child[i+1];
+				}
+				else {
+					cur = cur->child[i];
+				}
+			}
+			bool is_found = false;
+			for(int i=0; i<cur->num_keys; i++) {
+				if(value == cur->keys[i]) is_found = true;
+			}
+			if(is_found) {
+				delete_entry(cur, value);
+			}
+			else {
+				cout << "No such key" << endl;
+				return;
+			}
 		}
 		void printLeafNode(long long value) {
 			cout << "[INST]: PRINT_LEAF_NODE: " << value << endl;
